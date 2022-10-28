deef1e85b64fc1a99b918c578eea2b947156a9c9
Deoclecio Filho <deoclecio.filho@estudante.ufjf.br>
2022-10-27 22:25:59 -0300

    Arquivos criados

---
+++ src/main/java/padroesestruturais/bridge/Modelo.java
@@ -0,0 +1,25 @@
+package padroesestruturais.bridge;
+
+
+    public abstract class Modelo {
+
+        protected Versao versao;
+
+        protected float precoBase;
+
+        public Modelo(float precoBase) {
+            this.precoBase = precoBase;
+        }
+
+        public void setVersao(Versao versao) {
+            this.versao = versao;
+
+        }
+
+        public void setPrecoBase(float precoBase) {
+            this.precoBase = precoBase;
+
+        }
+
+        public abstract float calcularPreco();
+    }
